618e14a3bc9718c4dc8d757962702f5d1cdd3a49
leovc-f <leovicarv@gmail.com>
2025-11-01 17:22:16 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import map
+
+def main():
+    #Faz o Grid
+    grid = map.gera_grid()
+    
+    #Gera o labirinto
+    
+    map.gera_labirinto_dfs(grid, 1, 1)
+    
+    #Visualização
+    map.imprimir_grid(grid)
+
+if __name__ == "__main__":
+    main()
